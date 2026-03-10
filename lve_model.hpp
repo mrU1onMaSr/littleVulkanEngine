3e65213263e3c69a3e85bd76ce8556deaf2e4934
Yuvan Dutta <yuvandutta12@gmail.com>
2026-03-11 01:29:37 +0530

    Refactor code structure for improved readability and maintainability

--- lve_model.hpp
+++ lve_model.hpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <vector>
 #include <vulkan/vulkan_core.h>
+#include <memory>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -15,16 +16,27 @@ namespace lve {
         public:
 
         struct Vertex {
-            glm::vec3 position;
-            glm::vec3 color;
+            glm::vec3 position{};
+            glm::vec3 color{};
+            glm::vec3 normal{};
+            glm::vec2 uv{};
 
             static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+            bool operator==(const Vertex &other) const {
+                return position == other.position &&
+                       color == other.color &&
+                       normal == other.normal &&
+                       uv == other.uv;
+            }
         };
 
         struct Builder{
             std::vector<Vertex> vertices{};
             std::vector<uint32_t> indices{};
+
+            void loadModels(const std::string &filepath);
         };
 
         LveModel(LveDevice &device, const LveModel::Builder &builder);
@@ -32,6 +44,8 @@ namespace lve {
 
         LveModel(const LveWindow &) = delete;
         LveModel &operator=(const LveWindow &) = delete;
+
+        static std::unique_ptr<LveModel> createModelFromFile(LveDevice &device, const std::string &filepath);
 
         void bind(VkCommandBuffer commandBuffer);
         void draw(VkCommandBuffer commandBuffer);
